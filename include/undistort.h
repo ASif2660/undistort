13b3a6afbfa0cf373b10c17dde05a891d6c5f82a
ASif2660 <mdasifchand@gmail.com>
2018-04-26 22:02:20 +0000

    incldue added

---
+++ include/undistort.h
@@ -0,0 +1,20 @@
+#ifndef _UNDISTORT_H_
+#define _UNDISTORT_H_
+
+//method to read image file
+// TODO: extend this to ROS in future
+
+void readImage();
+
+static void help();
+
+
+
+
+
+
+
+
+
+
+#endif //undistort
