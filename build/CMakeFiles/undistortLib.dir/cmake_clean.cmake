f9540e6bab6d365e334d4334548e5a92e096d630
ASif2660 <mdasifchand@gmail.com>
2018-04-27 15:16:08 +0000

    push9ing build

---
+++ build/CMakeFiles/undistortLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/undistortLib.dir/src/undistort.cpp.o"
+  "libundistortLib.pdb"
+  "libundistortLib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/undistortLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
